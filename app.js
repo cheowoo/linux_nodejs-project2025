4e87bfc830c94a7f304d24705f18adbf25509ace
cheowoo <cjfdnfk369@gmail.com>
2025-08-12 17:05:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log("sssssssssss");
+const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// 인터프리트 언어라서 순서 중요.
+// todolist 목록에 사용할 임시 데이터 배열 (나중에 DB의 내용으로 바뀜)
+let todoList = [
+    {no:1, title: '학교 가기', done:false},
+    {no:2, title: '청소 가기', done:true},
+    {no:3, title: '친구와 놀러 가기', done:false}
+];
+let cnt = 4;
+
+// 뷰 템플릿 엔진 디렉토리 설정
+app.set('view engine', 'ejs'); // 확장자 (suffix)
+app.set('views', 'views'); // 경로 (prefix)
+
+// static 디렉토리 설정 (serve-static 미들웨어)
+app.use(express.static('public'));
+
+// post 방식의 요청 파라미터 데이터 전달을 위한 설정
+// body-parser 미들웨어
+app.use(express.json()); // JSON 형식의 요청 본문(body)를 파싱 (Ajax 요청 때)
+app.use(express.urlencoded({'extended':false})); 
+// HTML form 데이터 전송 처리 (application/x-www-form-urlencoded)
+
+
+// 패스 라우팅 설정
+app.get('/hello', (req, res) => {
+    res.end('<h1>Hello linux nodejs world</h1>');
+});
+
+app.get('/todos', (req, res) => {
+    // 목록 페이지로 이동 (키와 벨류가 동일하면 하나만 사용 가능)
+    req.app.render('todos', {todoList}, (err, html) => {
+        res.end(html);
+    });
+});
+
+app.post('/todos', (req, res) => {
+    // 새 할일 입력 폼에서 전달
+    console.log('POST - /todos');
+    //console.dir(req.body);
+    let newTodo = {
+        no: cnt++,
+        title: req.body.title,
+        done: (req.body.done=='true') ? true : false
+    }
+    // push(), splice(), findIndex()
+    todoList.push(newTodo);
+    res.redirect('/todos');
+});
+
+const server = http.createServer(app);
+server.listen(port, () => {``
+    console.log(`run on server, http://localhost:${port}`);
+});
